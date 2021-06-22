47d7eae6f5304bd1ebd006282b7e75edb147f02f
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-21 21:13:49 -0400

    adding the JS functionality

--- store.js
+++ store.js
@@ -5,5 +5,21 @@ if (document.readtState == 'loading') {
 }
 
 function ready() {
-    
+    let removeCartItemButtons = document.getElementsByClassName('btn-danger');
+    for (let i = 0; i < removeCartItemButtons.length; i++) {
+        let button = removeCartItemButtons[i];
+        button.addEventListener('click', removeCartItem);
+    };
+
+    let quantityInputs = document.getElementsByClassName('cart-quantity-input');
+    for (let i = 0; i < quantityInputs.length; i++) {
+        let input = quantityInputs[i];
+        input.addEventListener('change', quantityChanged);
+    };
+
+    let addToCartButtons = document.getElementsByClassName('shop-item-button');
+    for (let i = 0; i < addToCartButtons.length; i++) {
+        let button = addToCartButtons[i];
+        input.addEventListener('change', addToCartClicked);
+    };
 }
