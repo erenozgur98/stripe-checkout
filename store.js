514e3588f70d47be26c2c5cd0c6b5ee9b4fb9fee
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-22 10:45:06 -0400

    updateCartTotal is working

--- store.js
+++ store.js
@@ -12,19 +12,19 @@ function ready() {
         button.addEventListener('click', removeCartItem);
     };
 
-    let quantityInputs = document.getElementsByClassName('cart-quantity-input');
-    for (let i = 0; i < quantityInputs.length; i++) {
-        let input = quantityInputs[i];
-        input.addEventListener('change', quantityChanged);
-    };
+    // let quantityInputs = document.getElementsByClassName('cart-quantity-input');
+    // for (let i = 0; i < quantityInputs.length; i++) {
+    //     let input = quantityInputs[i];
+    //     input.addEventListener('change', quantityChanged);
+    // };
 
-    let addToCartButtons = document.getElementsByClassName('shop-item-button');
-    for (let i = 0; i < addToCartButtons.length; i++) {
-        let button = addToCartButtons[i];
-        input.addEventListener('change', addToCartClicked);
-    };
+    // let addToCartButtons = document.getElementsByClassName('shop-item-button');
+    // for (let i = 0; i < addToCartButtons.length; i++) {
+    //     let button = addToCartButtons[i];
+    //     input.addEventListener('change', addToCartClicked);
+    // };
 
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked);
+    // document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked);
 }
 
 function purchaseClicked() {
@@ -51,13 +51,19 @@ function addItemToCard(title, price, imageSrc) {
 
 function updateCartTotal() {
     let cartItemContainer = document.getElementsByClassName('cart-items')[0];
-    let cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    let cartRows = cartItemContainer.getElementsByClassName('cart-row');
+
+    let total = 0;
 
     for (let i = 0; i< cartRows.length; i++) {
         let cartRow = cartRows[i];
         let priceElement = cartRow.getElementsByClassName('cart-price')[0];
         let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
-        console.log(priceElement, quantityElement)
+        let price = parseFloat(priceElement.innerText.replace('$', ''));
+        let quantity = quantityElement.value;
+        total = total + (price * quantity);
     }
+
+    document.getElementsByClassName('cart-total-price')[0].innerText = `$${total}`;
 
 }
