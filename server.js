6c10f262428902fa34074d24ed9b6bf96df2fe0e
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-22 14:26:20 -0400

    adding /store

--- server.js
+++ server.js
@@ -7,8 +7,23 @@ const publicKey = process.env.STRIPE_PUBLIC_KEY;
 
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.get('/store', function(req, res) {
+    fs.readFile('items.json', function(err, data) {
+        if (err) {
+            res.status(500).end();
+        } else {
+            res.render('store.ejs', {
+                items: JSON.parse(data)
+            });
+        }
+    })
+})
+
+
 
 app.listen(3000);
